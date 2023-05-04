24afae5de5d0a92f9ab8279b722232ef774af2aa
Bellamy <84996163+g3y@users.noreply.github.com>
2023-05-04 04:46:20 +0200

    first commit
    should work

---
+++ src/app.js
@@ -0,0 +1,33 @@
+const fetch = require('node-fetch'),
+	{token} = require('../config.json');
+
+async function getUsername() {
+	const res = await fetch('https://discord.com/api/v9/users/@me/pomelo', {
+		method: 'POST',
+		headers: {
+			Authorization: token,
+			'Content-Type': 'application/json',
+			'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) discord/1.0.1027 Chrome/108.0.5359.215 Electron/22.3.2 Safari/537.36',
+		},
+		body: JSON.stringify({username: 'USERNAME_HERE'}),
+	});
+
+	switch (res.status) {
+		case 200:
+			console.log('(+) Username changed successfully');
+			break;
+		case 429:
+			console.log('(-) You are being rate limited');
+			sleep(Math.floor(Math.random() * (5500 - 3400 + 1) + 3400));
+			break;
+		default:
+			console.log('(-) An error occurred' + res.status);
+			sleep(Math.floor(Math.random() * (800 - 1400 + 1) + 1400));
+	}
+}
+
+getUsername();
+
+function sleep(ms) {
+	return new Promise((resolve) => setTimeout(resolve, ms));
+}
