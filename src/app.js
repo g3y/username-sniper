80363c027a79ddab4809f7cf232ada18b591991e
Bellamy <84996163+g3y@users.noreply.github.com>
2023-05-04 14:05:12 +0200

    forgot to loop it :skull:
    its really not the best code i made this in 10 mins tops

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const fetch = require('node-fetch'),
 	{token} = require('../config.json');
 
-async function getUsername() {
+async function snipeUsername() {
 	const res = await fetch('https://discord.com/api/v9/users/@me/pomelo', {
 		method: 'POST',
 		headers: {
@@ -18,16 +18,19 @@ async function getUsername() {
 			break;
 		case 429:
 			console.log('(-) You are being rate limited');
-			sleep(Math.floor(Math.random() * (5500 - 3400 + 1) + 3400));
 			break;
 		default:
 			console.log('(-) An error occurred ' + res.status);
-			sleep(Math.floor(Math.random() * (800 - 1400 + 1) + 1400));
+			break;
+	}
+	
+	return res.status;
+}
+
+async function main() {
+	while (true) {
+		const res = await snipeUsername();
 	}
 }
 
-getUsername();
-
-function sleep(ms) {
-	return new Promise((resolve) => setTimeout(resolve, ms));
-}
+main()
